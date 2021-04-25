8ccfd7513dc9936511103c1c9a46ccd1af261e7a
Aapok0 <aapo@kokot.fi>
2021-04-25 11:42:57 +0300

    Added navigation indicator of what page you are on

--- components/navbar.php
+++ components/navbar.php
@@ -1,4 +1,3 @@
-
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="container">
     <div class="navbar-brand">
@@ -13,32 +12,12 @@
       </a>
     </div>
 
+
     <div id="main_nav" class="navbar-menu">
       <div class="navbar-end">
-        <a class="navbar-item is-flex-direction-column" href="index.php">
-          <img class="mb-1" src="img/home.png" alt="Home">
-          Home
-        </a>
-
-        <a class="navbar-item is-flex-direction-column" href="about.php">
-          <img class="mb-1" src="img/about.png" alt="About">
-          About
-        </a>
-
-        <a class="navbar-item is-flex-direction-column" href="resume.php">
-          <img class="mb-1" src="img/resume.png" alt="Resume">
-          Resume
-        </a>
-
-        <a class="navbar-item is-flex-direction-column" href="skills.php">
-          <img class="mb-1" src="img/skills2.png" alt="Skills">
-          Skills
-        </a>
-
-        <a class="navbar-item is-flex-direction-column" href="interests.php">
-          <img class="mb-1" src="img/interests.png" alt="Interests">
-          Interests
-        </a>
+        <?php
+          include("php/navIndicator.php");
+        ?>
       </div>
     </div>
   </div>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include("components/navbar.php")
 <section class="hero is-fullheight has-background">
   <img class="hero-background is-transparent" src="img/header-background.jpeg" alt="Pältsan">
   <div class="hero-body">
-    <div class="container box has-text-centered background-3">
+    <div class="container hero-text box has-text-centered background-3">
       <h1 class="title">Hi! I'm Aapo Kokko.</h1>
       <h2 class="subtitle">Electrical engineer / Health tech student</h2>
       <p class="block">
