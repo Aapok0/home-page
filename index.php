4a1d0298e81a84bf5361d3a0aaacf661fd4f9631
Aapok0 <aapo@kokot.fi>
2021-04-26 21:32:39 +0300

    New fonts and resume-page content half done

--- index.php
+++ index.php
@@ -10,17 +10,17 @@ include("components/navbar.php")
 <section class="hero is-fullheight has-background">
   <img class="hero-background is-transparent" src="img/header-background.jpeg" alt="Pältsan">
   <div class="hero-body">
-    <div class="container hero-text box has-text-centered background-3">
-      <h1 class="title">Hi! I'm Aapo Kokko.</h1>
-      <h2 class="subtitle">Electrical engineer / Health tech student</h2>
-      <p class="block">
+    <div class="container hero-text box has-text-centered background-2">
+      <h1 class="title is-family-code">Hi! I'm Aapo Kokko.</h1>
+      <h2 class="subtitle is-family-code">Electrical engineer / Health tech student</h2>
+      <p class="block is-size-5">
         Aalto University graduate and a Metropolia student experienced in
         organization work and documentation with studies in multiple fields. 
         Seeking job summer/part-time opportunities in health technology, biomedical technology, 
         bioinformatics and life science. Also looking for thesis work starting early 2022.
       </p>
       <div class="block">
-        <a href="contacts.php"><button class="button is-link">Get in contact!</button></a>
+        <a href="contacts.php"><button class="button is-link has-text-weight-semibold">Get in contact!</button></a>
       </div>
     </div>
   </div>
--- resume.php
+++ resume.php
@@ -6,9 +6,87 @@ include("components/header.php")
 include("components/navbar.php")
 ?>
 
-<section class="section">
-  <div class="column box"></div>
-  <div class="column box"></div>
+<section class="section container">
+  <h1 class="title has-text-centered is-size-1">Resume</h1>
+  <div class="columns">
+    <div class="column box m-3 p-5 background-3">
+      <div class="box background-2">
+        <h1 class="title is-family-code">Education</h1>
+        <h2 class="subtitle is-family-code mt-1 mb-1">Bachelor of Engineering</h2>
+        <ul>
+          <li class="ml-3">Metropolia University of Applied Sciences</li>
+          <li class="ml-3">2020 - 2022</li>
+          <li class="ml-3">Programme: Information Technology</li>
+          <li class="ml-3">Major: Health and Well-being Technology</li>
+        </ul>
+
+        <h2 class="subtitle is-family-code mt-5 mb-1">Bachelor of Ecience in Technology</h2>
+        <ul>
+          <li class="ml-3">Aalto University</li>
+          <li class="ml-3">2012 - 2016</li>
+          <li class="ml-3">Programme: Electrical and Electronics Engineering</li>
+          <li class="ml-3">Major: Bionics</li>
+          <li class="ml-3">Minor: Information Technology</li>
+        </ul>
+      </div>
+
+      <div class="box background-2">
+        <h1 class="title is-family-code">Voluntary work</h1>
+        <h2 class="subtitle is-family-code mt-1 mb-1">Aalto University's Guild of Electrical Engineering</h2>
+        <ul>
+          <li>Halloped / Student Member in University's Administrative Committee</li>
+          <li class="ml-3">2016 - 2017 and 2017 - 2018</li>
+          <li class="ml-3">Multiple committees</li>
+          <li class="ml-3">Participated in meetings and voted for proposals</li>
+        </ul>
+        <ul class="mt-2">
+          <li>Council Member</li>
+          <li class="ml-3">2014 - 2015, 2015 - 2016 and 2017 - 2018</li>
+          <li class="ml-3">The guild council has the highest decision making power in the guild and it's members are chosen by the guild members in an election.</li>
+        </ul>
+        <ul class="mt-2">
+          <li>Writing and Graphic Layout</li>
+          <li class="ml-3">2017 - 2018</li>
+          <li class="ml-3">Writing and graphic layout for the guild's magazine Sössö</li>
+        </ul>
+        <ul class="mt-2">
+          <li>Tutor for Freshmen</li>
+          <li class="ml-3">2013 - 2014 and 2016 - 2017</li>
+          <li class="ml-3">Guidance to studies, general student life and guild activities</li>
+        </ul>
+        <ul class="mt-2">
+          <li>Chairman of the Studies Committee and a Member of the Guild Board</li>
+          <li class="ml-3">2016 - 2017</li>
+          <li class="ml-3">In addition to general guild board duties, I was in charge of the student advocacy of the guild members and electrical engineering students in general.</li>
+        </ul>
+        <ul class="mt-2">
+          <li>Member of the Studies Committee</li>
+          <li class="ml-3">2015 - 2016</li>
+          <li class="ml-3">Participated in the student advocacy of the guild members and electrical engineering students in general.</li>
+        </ul>
+        <ul class="mt-2">
+          <li>Member of the Corporate Relations Committee</li>
+          <li class="ml-3">2014 - 2015</li>
+          <li class="ml-3">Participated in building corporate relationships, organizing related events and gathering sponsors.</li>
+        </ul>
+      </div>
+    </div>
+    
+    <div class="column box m-3 p-5 background-3">
+      <div class="box background-2">
+        <h1 class="title is-family-code">Work experiences</h1>
+        <h2 class="subtitle is-family-code mt-1 mb-1">Office Secretary (IT)</h2>
+        <h2 class="subtitle is-family-code mt-5 mb-1">Documentation Specialist (Trainee)</h2>
+        <h2 class="subtitle is-family-code mt-5 mb-1">Newspaper Delivery</h2>
+        <h2 class="subtitle is-family-code mt-5 mb-1">ICT Technician</h2>
+        <h2 class="subtitle is-family-code mt-5 mb-1">Mover</h2>
+      </div>
+
+      <div class="box background-2">
+        <h1 class="title is-family-code">References</h1>
+      </div>
+    </div>
+  </div>
 </section>
 
 <?php
