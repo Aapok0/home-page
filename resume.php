740cc9c1c4a2971ca8ad09f5cf0c9dd99d86adca
Aapok0 <aapo@kokot.fi>
2021-04-27 08:59:41 +0300

    Resume written

--- resume.php
+++ resume.php
@@ -76,14 +76,61 @@ include("components/navbar.php")
       <div class="box background-2">
         <h1 class="title is-family-code">Work experiences</h1>
         <h2 class="subtitle is-family-code mt-1 mb-1">Office Secretary (IT)</h2>
+        <ul>
+          <li class="ml-3">Insurance Court of Finland</li>
+          <li class="ml-3"> 6.-7.2019, 2.-8.2020 and 9.-12.2020</li>
+          <li class="ml-3">Helping with the deployment, training and support of new software and general IT support and office work.</li>
+        </ul>
+
         <h2 class="subtitle is-family-code mt-5 mb-1">Documentation Specialist (Trainee)</h2>
+        <ul>
+          <li class="ml-3">Gemalto Oy (Thales Group)</li>
+          <li class="ml-3">8.2019 - 2.2020</li>
+          <li class="ml-3">Read through existing company documentation and supplier documentation and rewrote them into a new platform.</li>
+        </ul>
+
         <h2 class="subtitle is-family-code mt-5 mb-1">Newspaper Delivery</h2>
+        <ul>
+          <li class="ml-3">Posti Group Oyj</li>
+          <li class="ml-3">4.-5.2011, 4.-8.2012 and 6.-8.2013</li>
+          <li class="ml-3">Delivered newspapers during the night using a delivery bicycle.</li>
+        </ul>
+
         <h2 class="subtitle is-family-code mt-5 mb-1">ICT Technician</h2>
+        <ul>
+          <li class="ml-3">Elan IT Resource Oyj</li>
+          <li class="ml-3">3.2012</li>
+          <li class="ml-3">Searched channels for digiboxes, and if needed, updated them during a cable network update and answered any questions customers had about it.</li>
+        </ul>
+
         <h2 class="subtitle is-family-code mt-5 mb-1">Mover</h2>
+        <ul>
+          <li class="ml-3">Muuttohauta Ky</li>
+          <li class="ml-3">6.2009 - 3.2010</li>
+          <li class="ml-3">Included various moving jobs like moving people to new homes and moving furniture inside larger buildings like a school or a company (or out of it). </li>
+        </ul>
       </div>
 
       <div class="box background-2">
         <h1 class="title is-family-code">References</h1>
+        <h2 class="subtitle is-family-code mt-5 mb-1">Insurance Court of Finland</h2>
+        <ul>
+          <li>Paula Sorjonen</li>
+          <li class="ml-3">Phone: 029 56 43251</li>
+          <li class="ml-3">Email: paula.sorjonen@oikeus.fi</li>
+        </ul>
+
+        <h2 class="subtitle is-family-code mt-5 mb-1">Gemalto Oy (Thales Group)</h2>
+        <ul>
+          <li>Eduardo Hofmann</li>
+          <li class="ml-3">Phone: +358 505641231</li>
+          <li class="ml-3">Email: eduardo.hofmann@thalesgroup.com</li>
+        </ul>
+        <ul class="mt-2">
+          <li>Olli Rautiainen</li>
+          <li class="ml-3">Phone: +358 989414456</li>
+          <li class="ml-3">Email: olli.rautiainen@thalesgroup.com</li>
+        </ul>
       </div>
     </div>
   </div>
