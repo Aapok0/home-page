a22b29e59e3f5926a025efb836e2ba3c2acf001b
Aapo Kokko <aapo.kokko8@gmail.com>
2023-07-30 13:46:32 +0300

    Text left and image centered

--- about.php
+++ about.php
@@ -12,23 +12,23 @@ include(dirname(__FILE__).'/components/navbar.php')
   <!-- Hero box start -->
   <div class='hero-body'>
     <div class='container hero-text box has-text-centered background-1'>
-      <figure class="image is-128x128">
+      <figure class="image is-128x128 m-auto">
         <img class="is-rounded" src="img/portrait.jpg" alt="Portrait">
       </figure>
       <h1 class='title is-family-code'>About me</h1>
-      <p class='block is-size-5'>
+      <p class='block is-size-5 has-text-left'>
         Not exactly sure what flavor of engineer to call myself. I've studied electrical engineering, computer science and health technology and currently work with system administration and management of DevOps services on a consultancy basis. The work mostly revolves around solving issues, doing updates, setting up services and administration with a multitude of different tools and technologies. The company offers so many of them to the customers so it's not realistic to master all of them, but the ones I've worked with at least to certain extent include Linux servers, Ansible, Terraform, GitHub, GitLab, Bitbucket, Azure, Artifactory, Hashicorp Vault and scripting with Bash and Python.
       </p>
       <h2 class='subtitle is-family-code'>Why I enjoy problem solving</h2>
-      <p class='block is-size-5'>
+      <p class='block is-size-5 has-text-left'>
         I've begun to find the reasons why I enjoy problem solving so much, whether it be with programming, math, system administration, networking or any other avenue for it. To me problem solving feels like working as a detective.
       </p>
       <h2 class='subtitle is-family-code'>Why I directed my studies towards health technology</h2>
-      <p class='block is-size-5'>
+      <p class='block is-size-5 has-text-left'>
         Text here...
       </p>
       <h2 class='subtitle is-family-code'>What I want from the future</h2>
-      <p class='block is-size-5'>
+      <p class='block is-size-5 has-text-left'>
         Text here...
       </p>
     </div>
