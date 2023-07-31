5ab32eb80f0736bab3a5b7caa8628aa7fa6efcec
Aapo Kokko <aapo.kokko8@gmail.com>
2023-07-31 14:51:52 +0300

    Finished about text, button to resume, improved responsiveness with Bulma mixins and tweaks to index

--- index.php
+++ index.php
@@ -8,19 +8,23 @@ include(dirname(__FILE__).'/components/navbar.php')
 
 <!-- Hero banner, small introduction and call to action -->
 <section class='hero is-fullheight has-background'>
-  <img class='hero-background is-transparent' src='img/background.jpeg' alt='Background image of Pältsan'>
+  <img class='hero-background' src='img/background.jpeg' alt='Background image of Pältsan'>
   <!-- Hero box start -->
   <div class='hero-body'>
     <div class='container hero-text box has-text-centered background-1'>
-      <h1 class='title is-family-code'>Hi! I'm Aapo Kokko.</h1>
+
+      <!-- Short introduction -->
+      <h1 class='title is-family-code'>Hi! I'm <a href='about.php'>Aapo Kokko</a>.</h1>
       <h2 class='subtitle is-family-code'>DevOps engineer</h2>
       <p class='block is-size-5'>
         Aalto University and Metropolia graduate experienced in organization work, documentation and DevOps with studies in multiple fields. Certified in Github services and currently learning Full Stack development.
       </p>
+
       <!-- Call-to-action -->
       <div class='block'>
         <a href='#contact-info'><button class='button is-link has-text-weight-semibold'>Get in contact!</button></a>
       </div>
+
     </div>
   </div>
   <!-- Hero box end -->
